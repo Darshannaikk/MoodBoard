0c1875687c26e8ee16b9ce05871b7fc020ee10a9
Darshan Naik <naikdarshan1608@gmail.com>
2024-12-07 07:26:13 +0530

    Commit

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import "react-quill-new/dist/quill.snow.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Reflct",
+  title: "MoodBoard",
   description: "",
 };
 
@@ -42,7 +42,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-orange-300 py-12 bg-opacity-10">
             <div className="container mx-auto px-4 text-center text-gray-900">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Personal Project by Darshan Naik</p>
             </div>
           </footer>
         </body>
